01628ac141572cbf3a555ce2aad7e3b3df286c7d
Damm06 <ghkeka111@gmail.com>
2022-08-19 22:27:07 +0900

    1차 구현 완료

--- src/main/java/com/project/blog2/repository/BoardRepository.java
+++ src/main/java/com/project/blog2/repository/BoardRepository.java
@@ -1,8 +1,6 @@
 package com.project.blog2.repository;
 
 import com.project.blog2.domain.Board;
-import com.project.blog2.domain.User;
-import com.project.blog2.dto.board.BoardPostDto;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
 
